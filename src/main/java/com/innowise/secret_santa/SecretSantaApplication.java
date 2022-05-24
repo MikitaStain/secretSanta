95437e70e7db0df9f1ec255dd6ae6393146c4e99
MikitaStain <https://github.com/MikitaStain/internship.git>
2022-05-24 15:13:04 +0300

    created project secret Santa

---
+++ src/main/java/com/innowise/secret_santa/SecretSantaApplication.java
@@ -0,0 +1,11 @@
+package com.innowise.secret_santa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretSantaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SecretSantaApplication.class, args);
+    }
+}
